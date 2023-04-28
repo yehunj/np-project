09605c993e3b8e015940e869fb8a65c42d13bac5
Josh Kuesters <joshckuesters@gmail.com>
2023-04-27 21:24:39 -0400

    more stuff

--- approximate_solution/approx_TSP.py
+++ approximate_solution/approx_TSP.py
@@ -3,6 +3,24 @@ def approx_TSP():
 
 
 def main():
+    n, m = map(int, input().split())
+
+    correct_number_of_edges = n * (n - 1) // 2
+    if m != correct_number_of_edges:
+        raise ValueError('Invalid number of edges. Graph will not be complete.')
+
+    graph = {}
+
+    for i in range(m):
+        u, v, w = input().split()
+        if u not in graph:
+            graph[u] = {}
+        if v not in graph:
+            graph[v] = {}
+        graph[u][v] = float(w)
+        graph[v][u] = float(w)
+    
+    print (graph)
     pass
 
 if __name__ == '__main__':
