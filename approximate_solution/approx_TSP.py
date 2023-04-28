6634466c0c5b782498e8ae8365f830e0e781ff57
Ye Hun Joo <jooyx@dukes.jmu.edu>
2023-04-27 21:29:29 -0400

    approx

--- approximate_solution/approx_TSP.py
+++ approximate_solution/approx_TSP.py
@@ -1,5 +1,19 @@
-def approx_TSP():
-    pass
+import math
+
+def nearest_neighbor_tsp(graph):
+    start_vertex = list(graph.keys())[0]
+    current_vertex = start_vertex
+    unvisited_vertices = set(graph.keys())
+    unvisited_vertices.remove(start_vertex)
+    path = [start_vertex]
+    while unvisited_vertices:
+        nearest_neighbor = min(unvisited_vertices, key=lambda vertex: graph[current_vertex][vertex])
+        path.append(nearest_neighbor)
+        unvisited_vertices.remove(nearest_neighbor)
+        current_vertex = nearest_neighbor
+    path.append(start_vertex)
+    total_distance = sum([graph[path[i]][path[i+1]] for i in range(len(path)-1)])
+    return path, total_distance
 
 
 def main():
@@ -20,7 +34,10 @@ def main():
         graph[u][v] = float(w)
         graph[v][u] = float(w)
     
-    print (graph)
+    path, total_distance = nearest_neighbor_tsp(graph)
+    total_distance = int(total_distance)
+    print(total_distance)
+    print(' '.join(path))
     pass
 
 if __name__ == '__main__':
